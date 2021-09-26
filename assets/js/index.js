5161cd59d80804d731d92728437b3bdb4ce7256c
Basim <basim.abdullah247@gmail.com>
2021-09-26 15:33:17 +0500

    removed mini-game

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
 var color = ["#cccccc", "#ff0000", "#ff9900", "#ffcc66", "#ffff00", "#33ff33", "#66cccc", "#33ccff", "#6666cc", "#cc66cc", "#993300"];
 
 //11
-var idx = Math.floor(Math.random() * 11);
+/*var idx = Math.floor(Math.random() * 11);
 document.getElementById('test1').style.backgroundColor = color[idx];
 idx = Math.floor(Math.random() * 11);
 document.getElementById('test2').style.backgroundColor = color[idx];
 idx = Math.floor(Math.random() * 11);
-document.getElementById('test3').style.backgroundColor = color[idx];
+document.getElementById('test3').style.backgroundColor = color[idx];*/
