ae22ee37e9cb7320874c9e83046e16bf64f49716
talhasajjad07 <talhasajjad631@gmail.com>
2021-10-04 15:54:49 +0500

    for and while blocks

--- generators/javascript/myBlocks.js
+++ generators/javascript/myBlocks.js
@@ -34,3 +34,68 @@ Blockly.JavaScript['input'] = function(block) {
     var code = '{\n"commands":[' + statements_commands + '"end"]' + '\n' + "}";
     return code;
 };
+Blockly.JavaScript['repeat'] = function(block) {
+    var value_repeat = Blockly.JavaScript.valueToCode(block, 'repeat', Blockly.JavaScript.ORDER_ATOMIC);
+    var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
+    // TODO: Assemble JavaScript into code variable.
+    var statements_loop = statements_name;
+    for(var i=1; i<value_repeat;i++){
+        statements_loop = statements_loop + statements_name; 
+    }
+    if(value_repeat==0)
+    {
+        alert('kindly start from 1');
+        return '';
+    }
+    var code = `${statements_loop}`;
+    return code;
+}
+Blockly.JavaScript['for'] = function(block) {
+    var value_for = Blockly.JavaScript.valueToCode(block, 'for', Blockly.JavaScript.ORDER_ATOMIC);
+    var dropdown_condition = block.getFieldValue('condition');
+    var value_condition = Blockly.JavaScript.valueToCode(block, 'condition', Blockly.JavaScript.ORDER_ATOMIC);
+    var dropdown_increment = block.getFieldValue('increment');
+    var statements_statements = Blockly.JavaScript.statementToCode(block, 'statements');
+    // TODO: Assemble JavaScript into code variable.
+    var statements_loops = statements_statements;
+    if(dropdown_condition==">" && dropdown_increment=="++")
+    {
+        console.log('yes');
+        for(var i=value_for;i>drop_condition;dropdown_increment++)
+        {
+            statements_loops = statements_loops + statements_statements; 
+        }
+    }
+    // if(value_condition=='<' && dropdown_increment=='++')
+    // {
+    //     for(var i=value_for;i<value_condition;i=dropdown_increment)
+    //     {
+    //         statements_loops = statements_loops + statements_statements; 
+    //     }   
+    // }
+    // if(value_condition=='>' && dropdown_increment=='--')
+    // {
+    //     for(var i=value_for;i>value_condition;i=dropdown_increment)
+    //     {
+    //         statements_loops = statements_loops + statements_statements; 
+    //     }
+    // }
+    // if(value_condition=='<' && dropdown_increment=='--')
+    // {
+    //     for(var i=value_for;i<value_condition;i=dropdown_increment)
+    //     {
+    //         statements_loops = statements_loops + statements_statements; 
+    //     }
+    // }
+
+    var code = `${statements_loops}`;
+    return code;
+};
+
+Blockly.JavaScript['while'] = function(block) {
+    var dropdown_name = block.getFieldValue('NAME');
+    var value_number = Blockly.JavaScript.valueToCode(block, 'number', Blockly.JavaScript.ORDER_ATOMIC);
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return code;
+};
